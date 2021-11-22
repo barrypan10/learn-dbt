e4b8f17ca905b3028fa3a71b99d7d7989e14958b
Barry Pan <barry.pan@baruchmail.cuny.edu>
2021-11-22 01:35:24 +0000

    w

--- models/marts/core/int_orders_pivoted.sql
+++ models/marts/core/int_orders_pivoted.sql
@@ -1,19 +1,24 @@
-with payments as(
-    select * from {{red('stg_payments')}}
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
+
+with payments as (
+   select * from {{ ref('stg_payments') }}
 ),
 
-pivoted as(
-    select 
-        order_id,
-        sum(case when payment_method= 'bank_transfer' then amount else 0 end) as bank_transfer_ amount
-        sum(case when payment_method= 'coupon' then amount else 0 end) as coupon_ amount
-        sum(case when payment_method= 'credit_card' then amount else 0 end) as credit_card_ amount
-        sum(case when payment_method= 'gift_card' then amount else 0 end) as gift_card_ amount
+final as (
+   select
+       order_id,
+       {% for payment_method in payment_methods -%}
 
-    from payments
-    where status='success'
-    group by 1
+       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end)
+            as {{ payment_method }}_amount
 
+       {%- if not loop.last -%}
+         ,
+       {% endif -%}
+
+       {%- endfor %}
+   from {{ ref('stg_payments') }}
+   group by 1
 )
 
-select * from pivoted
+select * from final
